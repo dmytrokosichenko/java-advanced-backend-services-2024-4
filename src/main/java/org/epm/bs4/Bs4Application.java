e90e897749e1ef9c000fc8cf4776457ba2685697
Dmytro <dmytro.kosichenko@epam.com>
2024-03-07 20:10:01 +0100

    1. Add Spring boot cfg. 2. Add docker-compose for MySql

---
+++ src/main/java/org/epm/bs4/Bs4Application.java
@@ -0,0 +1,13 @@
+package org.epm.bs4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bs4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bs4Application.class, args);
+	}
+
+}
